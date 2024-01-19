4c8e997a6adfa68ee7e024595e9a9898c60ef493
LordeeQ <150349726+LordeeQ@users.noreply.github.com>
2024-01-20 00:19:35 +0300

    colors
    colors

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ pygame.init()
 width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("DD!Space")
+
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
