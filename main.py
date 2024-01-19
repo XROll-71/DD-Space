f83d39247afa51086cae3d7745328e5bb0f44576
DudukaAl <151047111+DudukaAl@users.noreply.github.com>
2024-01-20 00:22:33 +0300

    Update main.py
    sounds in projects

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ pygame.display.set_caption("DD!Space")
 white = (255, 255, 255)
 red = (255, 0, 0)
 blue = (0, 0, 255)
+
+pygame.mixer.init()
+
+# Загрузка музыки для главного экрана
+menu_music = pygame.mixer.Sound("data/menu_music.mp3")
+
+# Загрузка музыки для игрового процесса
+game_music = pygame.mixer.Sound("data/mainmenu.mp3")
+
+shoot_sound = pygame.mixer.Sound("data/shoot2.wav")
+
+explosion_sound = pygame.mixer.Sound("data/boom.wav")
+explosion_sound.set_volume(1.0)
