72f61ab6d2caccd0293620fb6289786b925c6e55
denis <nonametmate0257@gmail.com>
2024-01-20 00:14:19 +0300

    first code

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import pygame.mixer
+import sys
+import random
+import sqlite3
+
+
+width, height = 800, 600
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("DD!Space")
